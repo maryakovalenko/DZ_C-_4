4fe10b3e96e2ee08f4e71e228d1cb5291f4cbcb9
maryakovalenko <pomashka18061992@gmail.com>
2022-07-22 14:33:20 +0300

    Program.cs

---
+++ DZ_4_1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Функции
+Console.WriteLine("Задача № 25");
+Console.WriteLine("-------------------------------");
+double usePow (int A, int B) {
+   double result = Math.Pow(A, B);
+
+   return result; 
+}
+
+usePow(3,5);
+usePow(2,4);
