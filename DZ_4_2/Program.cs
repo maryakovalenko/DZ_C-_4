0acbd0145fe14632567d84f4b12fc142687263ab
maryakovalenko <pomashka18061992@gmail.com>
2022-07-22 14:57:26 +0300

    Program.cs

---
+++ DZ_4_2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Функции
+Console.WriteLine("Задача № 27 ");
+Console.WriteLine("-----------------------------");
+int digitsSum(int A) {
+    int sum = 0;
+    while(A != 0) {
+        sum = sum + A % 10;
+        A = A / 10;
+    }
+    
+    Console.WriteLine(sum);
+    return sum;
+        
+}
+
+int sum = digitsSum(452);
+digitsSum(82);
+digitsSum(9012);
