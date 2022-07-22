5d3ed78b4c4602a79bf7b73bc5849133c951cd00
maryakovalenko <pomashka18061992@gmail.com>
2022-07-22 15:28:55 +0300

    Program.cs

---
+++ DZ_4_3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Функции
+Console.WriteLine("Задача № 29");
+Console.WriteLine("-------------------------------");
+int lenght = 8;
+int[] digitsArr = new int[8];
+Random rnd = new Random();
+
+for (int i = 0; i < lenght; i++) {
+    digitsArr[i] = rnd.Next(-100, 100);
+    Console.Write(digitsArr[i] + ' ');
+}
